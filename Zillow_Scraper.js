5807e41280ccaf4c2660a4932e7f9b75b2f0bd40
Jacob McKay <jam486@byu.edu>
2025-10-27 23:14:22 -0600

    Create Zillow_Scraper.js

---
+++ Zillow_Scraper.js
@@ -0,0 +1,479 @@
+const missionInfo = {
+  apex: {
+    stakeName: "Apex",
+    apex: {
+      wardName: "Apex",
+      wardRegionId: "da6e164f67X1-CRetfw08drgjph_179s5p",
+      wardBoundary: "",
+    },
+    beavercreek: {
+      wardName: "Beaver Creek",
+      wardRegionId: "da6eb64f67X1-CRetfw08dselz9_179s5p",
+    },
+    harrislake: {
+      wardName: "Harris Lake",
+      wardRegionId: "da6f964f67X1-CRetfw08ds0dz9_179s5p",
+    },
+    hawriver: {
+      wardName: "Haw River",
+      wardRegionId: "da70064f67X1-CRetfw08dthl8l_179s5p",
+    },
+    pittsboro: {
+      wardName: "Pittsboro",
+      wardRegionId: "da70864f67X1-CReth9w6ajwmv9_179s5p",
+    },
+    sanford: {
+      wardName: "Sanford",
+      wardRegionId: "da70d64f67X1-CRetfw08dt6shx_179s5p",
+    },
+    silercity: {
+      wardName: "Siler City",
+      wardRegionId: "6fd4664f77X1-CRybk5u7f0d51h_1dij59",
+    },
+    whiteoak: {
+      wardName: "White Oak",
+      wardRegionId: "6fd5064f77X1-CRybljq5bt5611_1dij59",
+    },
+  },
+  durham: {
+    stakeName: "Durham",
+    chapelhill1: {
+      wardName: "Chapel Hill 1st",
+      wardRegionId: "b38e664fcdX1-CRtovsl2q3mw1x_18y6m0",
+    },
+    chapelhill2: {
+      wardName: "Chapel Hill 2nd",
+      wardRegionId: "e3f6f64fcfX1-CR1bcshtp2ze0ud_1blpa4",
+    },
+    durham1: {
+      wardName: "Durham 1st",
+      wardRegionId: "e3f7f64fd0X1-CR1bcshtp319g3p_1blpa4",
+    },
+    durham2: {
+      wardName: "Durham 2nd",
+      wardRegionId: "e3f8464fd0X1-CR1bctvpmzu64lh_1blpa4",
+    },
+    durham4: {
+      wardName: "Durham 4th",
+      wardRegionId: "e3f9164fd0X1-CR1bctvpmzvezlx_1blpa4",
+    },
+    hillsborough: {
+      wardName: "Hillsborough",
+      wardRegionId: "e3f9c64fd0X1-CR1bcshtp33ymrp_1blpa4",
+    },
+    mebane: {
+      wardName: "Mebane",
+      wardRegionId: "e3fa564fd0X1-CR1bcshtp35lps5_1blpa4",
+    },
+    roxboro: {
+      wardName: "Roxboro",
+      wardRegionId: "e3fbb64fd0X1-CR1bctvpmznub45_1blpa4",
+    },
+    southboston: {
+      wardName: "South Boston",
+      wardRegionId: "e3fc764fd0X1-CR1bcshtp2y9q6d_1blpa4",
+    },
+  },
+  fayetteville: {
+    stakeName: "Fayetteville",
+    clinton: {
+      wardName: "Clinton",
+      wardRegionId: "e3fd264fd0X1-CR1bctvpmzplio5_1blpa4",
+    },
+    fayetteville1: {
+      wardName: "Fayetteville 1st",
+      wardRegionId: "e3fe064fd0X1-CR1bcshtp30n8hx_1blpa4",
+    },
+    fayetteville4: {
+      wardName: "Fayetteville 4th",
+      wardRegionId: "e3fea64fd0X1-CR1bctvpmzrk3ph_1blpa4",
+    },
+    gillishill: {
+      wardName: "Gillis Hill",
+      wardRegionId: "e0dd064fe4X1-CRb7kf5bq0rdtx_1bd9va",
+    },
+    hopemills: {
+      wardName: "Hope Mills",
+      wardRegionId: "e0dd864fe4X1-CRb7lt19mrr5c5_1bd9va",
+    },
+    lumberton: {
+      wardName: "Lumberton",
+      wardRegionId: "e0de164fe4X1-CRb7lt19mtmb45_1bd9va",
+    },
+  },
+  fayettevillewest: {
+    stakeName: "Fayetteville West",
+    andersoncreekpark: {
+      wardName: "Anderson Creek Park",
+      wardRegionId: "e0dfb64fe4X1-CRb7kf5bq355mt_1bd9va",
+    },
+    cameron: {
+      wardName: "Cameron",
+      wardRegionId: "e0dfe64fe5X1-CRb7kf5bq56jt1_1bd9va",
+    },
+    fayetteville3: {
+      wardName: "Fayetteville 3rd",
+      wardRegionId: "e0e0364fe5X1-CRb7lt19mwcq11_1bd9va",
+    },
+    laurinburg: {
+      wardName: "Laurinburg",
+      wardRegionId: "e0e2764fe5X1-CRb7kf5bq763lx_1bd9va",
+    },
+    pembroke: {
+      wardName: "Pembroke",
+      wardRegionId: "e0e2f64fe5X1-CRb7lt19mzo7gl_1bd9va",
+    },
+    pinehurst: {
+      wardName: "Pinehurst",
+      wardRegionId: "e0e3564fe5X1-CRb7kf5bq8anr9_1bd9va",
+    },
+    rockingham: {
+      wardName: "Rockingham",
+      wardRegionId: "e0e4064fe5X1-CRb7lt19mzirid_1bd9va",
+    },
+    southernpines: {
+      wardName: "Southern Pines",
+      wardRegionId: "e0e4764fe5X1-CRb7lt19mqy01x_1bd9va",
+    },
+    westover: {
+      wardName: "Westover",
+      wardRegionId: "e0e4964fe5X1-CRb7kf5bq0dfb9_1bd9va",
+    },
+  },
+  greenville: {
+    stakeName: "Greenville",
+    greenville1: {
+      wardName: "Greenville 1st",
+      wardRegionId: "f6d0964ffbX1-CR1jd127e0ktih1_17uuos",
+    },
+    greenville2: {
+      wardName: "Greenville 2nd",
+      wardRegionId: "f6d0e64ffbX1-CR1jd2g3bxb462t_17uuos",
+    },
+    kinston: {
+      wardName: "Kinston",
+      wardRegionId: "f6d1464ffbX1-CR1jd127e0kk151_17uuos",
+    },
+    roanokerapids: {
+      wardName: "Roanoke Rapids",
+      wardRegionId: "f6d1764ffbX1-CR1jd2g3bxctpl1_17uuos",
+    },
+    rockymount: {
+      wardName: "Rocky Mount",
+      wardRegionId: "f6d1f64ffbX1-CR1jd127e0mf6x1_17uuos",
+    },
+    washington: {
+      wardName: "Washington",
+      wardRegionId: "f6d2364ffbX1-CR1jd2g3bxc7bnp_17uuos",
+    },
+    wilson: {
+      wardName: "Wilson",
+      wardRegionId: "f6d2764ffbX1-CR1jd127e0lorlx_17uuos",
+    },
+  },
+  goldsboro: {
+    stakeName: "Goldsboro",
+    albertson: {
+      wardName: "Albertson",
+      wardRegionId: "c215165078X1-CRcndlw144swcl_17ntuf",
+    },
+    clayton: {
+      wardName: "Clayton",
+      wardRegionId: "c217065078X1-CRcndlw14802x1_17ntuf",
+    },
+    cleveland: {
+      wardName: "Cleveland",
+      wardRegionId: "c217d65078X1-CRcndlw1485yo5_17ntuf",
+    },
+    goldsboro1: {
+      wardName: "Goldsboro 1st",
+      wardRegionId: "c217f65078X1-CRcnezrz0y666d_17ntuf",
+    },
+    goldsboro2: {
+      wardName: "Goldsboro 2nd",
+      wardRegionId: "c218465078X1-CRcndlw14952v9_17ntuf",
+    },
+    mountolive: {
+      wardName: "Mount Olive",
+      wardRegionId: "c218965078X1-CRcndlw149k05h_17ntuf",
+    },
+    smithfield: {
+      wardName: "Smithfield",
+      wardRegionId: "c219065078X1-CRcndlw148mn5x_17ntuf",
+    },
+    woodington: {
+      wardName: "Woodington",
+      wardRegionId: "c219465078X1-CRcnezrz0zhubp_17ntuf",
+    },
+  },
+  moreheadcity: {
+    stakeName: "Morehead City",
+    camplejeune: {
+      wardName: "Camp Lejeune",
+      wardRegionId: "3aca0650e1X1-CRmptvyj1uign9_1bkapn",
+    },
+    harkersisland: {
+      wardName: "Harkers Island",
+      wardRegionId: "3aca2650e1X1-CRmpv9ugykbqh1_1bkapn",
+    },
+    havelock: {
+      wardName: "Havelock",
+      wardRegionId: "3acaf650e1X1-CRmptvyj1v7hdx_1bkapn",
+    },
+    jacksonville2: {
+      wardName: "Jacksonville 2nd",
+      wardRegionId: "3aca7650e1X1-CRmptvyj1tosed_1bkapn",
+    },
+    jacksonville3: {
+      wardName: "Jacksonville 3rd",
+      wardRegionId: "8dcab6500fX1-CR1eigoyapp4m9x_199f5s",
+    },
+    moreheadcity: {
+      wardName: "Morehead City",
+      wardRegionId: "3acbe650e1X1-CRmpv9ugyme3et_1bkapn",
+    },
+    newbern: {
+      wardName: "New Bern",
+      wardRegionId: "3acc3650e1X1-CRmptvyj1umi11_1bkapn",
+    },
+  },
+  raleigh: {
+    stakeName: "Raleigh",
+    bondpark: {
+      wardName: "Bond Park",
+      wardRegionId: "0c9d164b98X1-CRv3023o0cmj7p_1edfnn",
+    },
+    cary1: {
+      wardName: "Cary 1st",
+      wardRegionId: "3acd0650e2X1-CRmptvyj1vy9c5_1bkapn",
+    },
+    greenlevel: {
+      wardName: "Green Level",
+      wardRegionId: "3acd9650e2X1-CRmpv9ugyp1m11_1bkapn",
+    },
+    morrisville: {
+      wardName: "Morrisville",
+      wardRegionId: "3ace0650e2X1-CRmpv9ugyogszp_1bkapn",
+    },
+    raleigh1: {
+      wardName: "Raleigh 1st",
+      wardRegionId: "30346646e3X1-CRni8nhady8p1h_1fz0z0"
+    },
+    raleigh2: {
+      wardName: "Raleigh 2nd",
+      wardRegionId: "3034c646e3X1-CRni8nhadyjuf9_1fz0z0"
+    },
+    raleigh4: {
+      wardName: "Raleigh 4th",
+      wardRegionId: "30364646e3X1-CRnia1d8aqadvp_1fz0z0"
+    },
+  },
+  raleighsouth: {
+    stakeName: "Raleigh South",
+    dunn: {
+      wardName: "Dunn",
+      wardRegionId: "3ace5650e2X1-CRmpv9ugypvjr9_1bkapn",
+    },
+    fuquayvarina: {
+      wardName: "Fuquay-Varina",
+      wardRegionId: "3ace8650e2X1-CRmptvyj1zdyh1_1bkapn",
+    },
+    garner1: {
+      wardName: "Garner 1st",
+      wardRegionId: "3aced650e2X1-CRmptvyj1yenyd_1bkapn",
+    },
+    hollysprings: {
+      wardName: "Holly Springs",
+      wardRegionId: "3acf3650e2X1-CRmptvyj20eked_1bkapn",
+    },
+    lakewheeler: {
+      wardName: "Lake Wheeler",
+      wardRegionId: "3acf6650e2X1-CRmpv9ugyrkts5_1bkapn",
+    },
+    sunsetlake: {
+      wardName: "Sunset Lake",
+      wardRegionId: "3acfa650e2X1-CRmptvyj1zkw5h_1bkapn",
+    },
+    swiftcreek: {
+      wardName: "Swift Creek",
+      wardRegionId: "3ad03650e2X1-CRmpv9ugyihw3p_1bkapn",
+    },
+  },
+  wakeforest: {
+    stakeName: "Wake Forest",
+    creedmoor: {
+      wardName: "Creedmoor",
+      wardRegionId: "b277c650f8X1-CR8lrb5ejecpmt_1dqyk3",
+    },
+    fallslake: {
+      wardName: "Falls Lake",
+      wardRegionId: "b2783650f8X1-CR8lpx9gmokxj9_1dqyk3",
+    },
+    franklinton: {
+      wardName: "Franklinton",
+      wardRegionId: "b2787650f8X1-CR8lpx9gmonkcl_1dqyk3",
+    },
+    henderson: {
+      wardName: "Henderson",
+      wardRegionId: "b2790650f8X1-CR8lrb5ejf383p_1dqyk3",
+    },
+    knightdale1: {
+      wardName: "Knightdale 1st",
+      wardRegionId: "b2795650f8X1-CR8lpx9gmq3g79_1dqyk3",
+    },
+    wakeforest1: {
+      wardName: "Wake Forest 1st",
+      wardRegionId: "b279a650f8X1-CR8lrb5ejhs5ad_1dqyk3",
+    },
+    wakeforest2: {
+      wardName: "Wake Forest 2nd",
+      wardRegionId: "b279c650f8X1-CR8lrb5ejh7lqd_1dqyk3",
+    },
+    zebulon: {
+      wardName: "Zebulon",
+      wardRegionId: "b27a0650f8X1-CR8lrb5ej94b7p_1dqyk3",
+    },
+  },
+  wilmington: {
+    stakeName: "Wilmington",
+    hampstead: {
+      wardName: "Hampstead",
+      wardRegionId: "6fa8b64c54X1-CRh67djiamv1dx_1a9xy2",
+    },
+    hollyridge: {
+      wardName: "Holly Ridge",
+      wardRegionId: "b38db64fcdX1-CRtox6h0n3c2it_18y6m0",
+    },
+    leland: {
+      wardName: "Leland",
+      wardRegionId: "b38d764fcdX1-CRtovsl2qd8p8l_18y6m0",
+    },
+    southport: {
+      wardName: "Southport",
+      wardRegionId: "5294464f0bX1-CR3ui0zq8cwjid_16vqgz",
+    },
+    wallace: {
+      wardName: "Wallace",
+      wardRegionId: "05e1864e79X1-CRjet5rw32iu2t_1b690x",
+    },
+    wilmington1: {
+      wardName: "Wilmington 1st",
+      wardRegionId: "5293e64f0bX1-CR3ujevo5cd491_16vqgz",
+    },
+    wilmington2: {
+      wardName: "Wilmington 2nd",
+      wardRegionId: "5293264f0bX1-CR3ui0zq8lzttx_16vqgz",
+    },
+  },
+};
+
+
+var currentStake;
+var currentWard;
+const bullet = "\u2B9E";
+
+
+const doIt = () => getStake();
+
+
+const stringify = s => {
+  s = s || "";
+  return s.toLowerCase().replace(/"| |-/g, "");
+};
+
+
+const capitalize = s => stringify(s).charAt(0).toUpperCase() + stringify(s).slice(1);
+
+
+const getStakeName = s => missionInfo[s].stakeName;
+
+
+const getWardName = w => missionInfo[currentStake][w].wardName;
+
+
+const getStake = () => {
+  let stakeList = Object.keys(missionInfo).map(getStakeName).join(`\n${bullet} `);
+  let promptStake = prompt(`Which stake are you in? Available stake are:\n${bullet} ${stakeList}`);
+  currentStake = stringify(promptStake).replace(/stake/gi, "");
+  if (promptStake == null) { }
+  else if (!(currentStake in missionInfo)) {
+    alert(`I don't recognize the ${promptStake} Stake. Please try again`);
+    getStake();
+  }
+  else if (confirm(`I know that Stake! Continue with the ${getStakeName(currentStake)} Stake?`)) { getWard() }
+  else { getStake() }
+};
+
+
+const getWard = () => {
+  let wards = Object.keys(missionInfo[currentStake]);
+  if (wards.shift() != "stakeName") { console.error("Welp this code is bunk") };
+  let wardList = wards.map(getWardName).join(`\n${bullet} `);
+  let promptWard = prompt(`Which ward (or branch) are you in? Select One:\n${bullet} ${wardList}`);
+  currentWard = stringify(promptWard).replace(/ward|branch/gi, "").replace(/1st/gi, "1").replace(/2nd/gi, "2").replace(/3rd/gi, "3").replace(/4th/gi, "4").replace(/5th/gi, "5");
+  if (promptWard == null) { getStake() }
+  else if (!(currentWard in missionInfo[currentStake])) {
+    alert(`I don't recognize the ${promptWard} Ward. Please try again`);
+    getWard(currentStake);
+  }
+  else if (confirm(`I know that Ward! Continue as the ${getWardName(currentWard)} Ward (in the ${getStakeName(currentStake)} Stake)?`)) {
+    getDays(missionInfo[currentStake][currentWard].wardRegionId);
+  }
+  else { getWard(currentStake) }
+};
+
+
+const getDays = (upRegion) => {
+  let promptDays = prompt("Would you like zillows from the last 14, 30, or 90 days?");
+  let days = stringify(promptDays).replace("fourteen", "14").replace("thirty", "30").replace("ninety", "90");
+  if (promptDays == null) { getWard(currentStake) }
+  else if (days == "14" || days == "30" || days == "90") {
+    getZillows(upRegion, days);
+    alert(`Grabbing zillows from the last ${days} days. This may take a second to load. Then they'll (hopefully) be copied to your clipboard.`);
+  }
+  else {
+    alert(`I didn't recognize the number you inputted...`);
+    getDays(upRegion);
+  }
+};
+
+
+const getZillows = async (upRegionId, upDays) => {
+  const baseQueryState = `{"mapBounds":{"west":-80,"east":-70,"south":30,"north":40},"isMapVisible":true,"filterState":{"sortSelection":{"value":"days"},"isForSaleByAgent":{"value":false},"isForSaleByOwner":{"value":false},"isNewConstruction":{"value":false},"isForSaleForeclosure":{"value":false},"isComingSoon":{"value":false},"isAuction":{"value":false},"isRecentlySold":{"value":true},"doz":{"value":"90"},"isAllHomes":{"value":true},"isListVisible":true,"pagination":{"currentPage":1},"mapZoom":1,"customRegionId":"c90d6646d5X1-CR1b03aydesuh8l_1f5j13"}}`;
+  var searchObj = JSON.parse(baseQueryState);
+  searchObj.filterState.doz = { "value": upDays };
+  if (upRegionId != null && upRegionId != "") { searchObj.customRegionId = upRegionId };
+  const searchQueryState = JSON.stringify(searchObj);
+  const searchBase = `https://www.zillow.com/search/GetSearchPageState.htm`;
+  const wants = `{"cat1":["listResults","mapResults"]}`;
+  const requestId = "1";
+  let search = new URLSearchParams({ searchQueryState, wants, requestId });
+  let initRequest = await fetch(`${searchBase}?${search}`);
+  let initMapData = await initRequest.json();
+  let pageCount = initMapData.cat1.searchList.totalPages;
+  let initHomes = initMapData.cat1.searchResults.listResults;
+  const homes = initHomes.map((h) => h.address);
+  searchObj = JSON.parse(searchQueryState);
+  for (var i = pageCount; i >= 2; i--) {
+    searchObj.pagination = { currentPage: i };
+    search = new URLSearchParams({ searchQueryState: JSON.stringify(searchObj), wants, requestId });
+    let mapRequest = await fetch(`${searchBase}?${search}`);
+    let mapData = await mapRequest.json();
+    let pageHomes = mapData.cat1.searchResults.listResults;
+    let addresses = pageHomes.map((h) => h.address);
+    homes.push(...addresses);
+  };
+  await copyAddresses(homes);
+  console.log(`You're all done! ${homes.length} homes.`)
+};
+
+const copyAddresses = async (homes) => {
+  try {
+    await navigator.clipboard.writeText(homes.join("\n"));
+    alert(`You had ${homes.length} homes copied to your clipboard! Now you can paste them into a spreasheet and the I would reccomend you copy it from there into a saved location in google maps so you can see them all!`);
+  }
+  catch (err) {
+    alert("I couldn't access your clipboard. I'll log the addresses in the console that you pasted the code in and you should be able to copy it from there. Then you can paste them into a spreadsheet, and from there I would reccomend you save them as a saved location in google maps so you can see them all.");
+    console.log(homes.join("\n"));
+  }
+};
